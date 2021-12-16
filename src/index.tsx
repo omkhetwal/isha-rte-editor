9a6983b5e0907f5c6d0e161020a6fcd1fb203050
omkhetwal <khetwalom@gmail.com>
2021-12-16 18:32:50 +0530

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,127 @@
+// @ts-nocheck
+
+// eslint-disable-next-line no-unused-vars
+import EditorJS from '@editorjs/editorjs';
+import Embed from '@editorjs/embed';
+import List from '@editorjs/list';
+import SimpleImage from '@editorjs/simple-image';
+import Paragraph from 'editorjs-paragraph-with-alignment';
+import ColorPlugin from 'editorjs-text-color-plugin';
+import * as React from 'react';
+import { useCallback, useEffect, useImperativeHandle, useRef } from 'react';
+import fontSizePlugin from './plugins/fontSize';
+import Video from './plugins/video';
+import './style.css';
+
+interface IProps {
+  onChange: (a: any) => void;
+  containerClass?: string;
+  editorId: string;
+}
+
+const tools = {
+  paragraph: {
+    class: Paragraph,
+    inlineToolbar: true,
+  },
+
+  list: {
+    class: List,
+    inlineToolbar: true,
+  },
+  Color: {
+    class: ColorPlugin,
+    config: {
+      colorCollections: [
+        '#FF1300',
+        '#EC7878',
+        '#9C27B0',
+        '#673AB7',
+        '#3F51B5',
+        '#0070FF',
+        '#03A9F4',
+        '#00BCD4',
+        '#4CAF50',
+        '#8BC34A',
+        '#CDDC39',
+        '#FFF',
+        '#000',
+      ],
+      defaultColor: '#FF1300',
+      type: 'text',
+    },
+  },
+  // textAlign: TextAlign,
+  embed: Embed,
+  image: SimpleImage,
+  video: Video,
+  fontSize: fontSizePlugin,
+};
+
+const RteEditor = (props: IProps, ref: any) => {
+  const { onChange, containerClass, editorId } = props;
+
+  const editorRef = useRef<any>(null);
+
+  useImperativeHandle(ref, () => ({
+    loadData: (data) => {
+      // setEditorData(data);
+      if (editorRef?.current) {
+        editorRef?.current?.destroy();
+      }
+      const editor = new EditorJS({
+        holder: editorId,
+        data: data,
+        onReady: () => {
+          editorRef.current = editor;
+          editor.saver.save();
+        },
+        onChange: async () => {
+          const content = await editor.saver.save();
+          onChange(content);
+        },
+        autofocus: true,
+        tools,
+      });
+    },
+  }));
+
+  const initEditor = useCallback(() => {
+    const editor = new EditorJS({
+      holder: editorId,
+      data: null,
+      onReady: () => {
+        editorRef.current = editor;
+      },
+      onChange: async () => {
+        const content = await editor.saver.save();
+        onChange(content);
+      },
+      autofocus: true,
+      tools,
+    });
+  }, []);
+
+  useEffect(() => {
+    //destroy if exists
+    if (!editorRef.current) {
+      initEditor();
+    }
+    return () => {
+      console.log('un mounting');
+      // eslint-disable-next-line no-unused-expressions
+      if (editorRef?.current) {
+        editorRef?.current?.destroy?.();
+      }
+      editorRef.current = undefined;
+    };
+  }, []);
+
+  return (
+    <div className={`${containerClass ? containerClass : 'defaultContainer'} `}>
+      <div className={'editorHolder'} id={editorId} />
+    </div>
+  );
+};
+
+export default React.forwardRef(RteEditor);
---
+++ src/plugins/video/index.ts
@@ -0,0 +1,75 @@
+import './index.css';
+
+export default class Video {
+  data: { hls: string; dash: string; youtube: string; url: string };
+  nodes: { container: any; hlsInput: any; dashInput: any; youtubeInput: any };
+  constructor() {
+    this.data = {
+      hls: '',
+      dash: '',
+      youtube: '',
+      url:
+        'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+    };
+    this.nodes = {
+      container: null,
+      hlsInput: null,
+      dashInput: null,
+      youtubeInput: null,
+    };
+  }
+
+  // @ts-ignore
+  static get toolbox() {
+    return {
+      title: 'Stream',
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" style="width:20px;height:20px" viewBox="0 0 20 20" fill="currentColor">
+            <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
+          </svg>`,
+    };
+  }
+
+  render() {
+    const container = document.createElement('div');
+    const containerStyles = {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      backgroundColor: '#efefef',
+      zIndex: '20',
+      padding: '24px',
+      margin: '.5rem',
+    };
+
+    Object.assign(container.style, containerStyles);
+
+    const hlsBox = document.createElement('input');
+    hlsBox.placeholder = 'HLS Source';
+    this.nodes.hlsInput = hlsBox;
+
+    const dashBox = document.createElement('input');
+    dashBox.placeholder = 'DASH Source';
+    this.nodes.dashInput = dashBox;
+
+    const youtubeBox = document.createElement('input');
+    youtubeBox.placeholder = 'YOUTUBE Source';
+    this.nodes.youtubeInput = youtubeBox;
+
+    hlsBox.classList.add('url-input');
+    dashBox.classList.add('url-input');
+    youtubeBox.classList.add('url-input');
+    container.appendChild(hlsBox);
+    container.appendChild(dashBox);
+    container.appendChild(youtubeBox);
+    return container;
+  }
+
+  save() {
+    this.data.dash = this.nodes.dashInput.value;
+    this.data.hls = this.nodes.hlsInput.value;
+    this.data.youtube = this.nodes.youtubeInput.value;
+    return {
+      ...this.data,
+    };
+  }
+}
